7f857df132a303116c693c04f2cf25fb0bdd900f
kaiumuddin <kaium.uddin2909@gmail.com>
2024-09-21 14:40:23 +0600

    changed render sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:migrate
+bundle exec rails db:seed
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
